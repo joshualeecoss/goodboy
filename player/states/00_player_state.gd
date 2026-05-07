471a9758e8858ee7e549c8f79be29484195502d5
Josh Coss <josh.coss@gmail.com>
2026-05-07 06:14:30 -0600

    State Machine Setup
    Including scripts for Idle and Run states

---
+++ player/states/00_player_state.gd
@@ -0,0 +1,37 @@
+@icon("res://player/states/state.svg")
+class_name PlayerState extends Node
+
+var player : Player
+var next_state : PlayerState
+
+#region /// state references
+# reference to all other states
+#endregion
+
+
+# What happens when this state is initialized?
+func init() -> void:
+	pass
+
+
+# What happens when we enter this state
+func enter() -> void:
+	pass
+
+
+# What happens when we exit this state
+func exit() -> void:
+	pass
+
+
+# What happens when an input is pressed (or released)
+func handle_inputs(_event: InputEvent) -> PlayerState:
+	return next_state
+
+
+func process(_delta: float) -> PlayerState:
+	return next_state
+
+
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
---
+++ player/states/idle.gd
@@ -0,0 +1,29 @@
+class_name PlayerStateIdle extends PlayerState
+
+
+# What happens when this state is initialized?
+func init() -> void:
+	pass
+
+
+# What happens when we enter this state
+func enter() -> void:
+	pass
+
+
+# What happens when we exit this state
+func exit() -> void:
+	pass
+
+
+# What happens when an input is pressed (or released)
+func handle_inputs(_event: InputEvent) -> PlayerState:
+	return next_state
+
+
+func process(_delta: float) -> PlayerState:
+	return next_state
+
+
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
